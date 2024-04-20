f0b7df6891f9d83ab415d5da91e85b5d6cce1b88
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2024-04-20 08:32:57 +0000

    fix(deps): update terraform-module

--- solutions/secure/main.tf
+++ solutions/secure/main.tf
@@ -1,6 +1,6 @@
 module "resource_group" {
   source                       = "terraform-ibm-modules/resource-group/ibm"
-  version                      = "1.1.4"
+  version                      = "1.1.5"
   resource_group_name          = var.existing_resource_group == false ? var.resource_group_name : null
   existing_resource_group_name = var.existing_resource_group == true ? var.resource_group_name : null
 }
@@ -12,7 +12,7 @@ module "kms" {
   }
   count                       = var.existing_kms_key_crn != null ? 0 : 1 # no need to create any KMS resources if passing an existing key
   source                      = "terraform-ibm-modules/kms-all-inclusive/ibm"
-  version                     = "4.8.5"
+  version                     = "4.9.1"
   create_key_protect_instance = false
   region                      = var.kms_region
   existing_kms_instance_guid  = var.existing_kms_instance_guid
