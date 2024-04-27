2e3fb6d024bfad8c586a90b4e87cc5f2a9b970be
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2024-04-27 04:00:13 +0100

    chore(deps): update terraform terraform-ibm-modules/secrets-manager/ibm to v1.11.0 (#149)

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -89,7 +89,7 @@ resource "elasticsearch_cluster_settings" "global" {
 # Create Secrets Manager Instance (if not using existing one)
 module "secrets_manager" {
   source               = "terraform-ibm-modules/secrets-manager/ibm"
-  version              = "1.10.0"
+  version              = "1.11.0"
   resource_group_id    = module.resource_group.resource_group_id
   region               = var.region
   secrets_manager_name = "${var.prefix}-secrets-manager"
