006ef0cd88bad2af0ec328d1e15a1f2e5992ab1b
Soaib024 <Soaib024@gmail.com>
2024-06-07 19:16:51 +0530

    fixed pre-commit

--- tests/pr_test.go
+++ tests/pr_test.go
@@ -58,10 +58,10 @@ func TestRunFSCloudExample(t *testing.T) {
 		*/
 		//ResourceGroup: resourceGroup,
 		TerraformVars: map[string]interface{}{
-			"elasticsearch_version":     "8.12", // Always lock this test into the latest supported elasticsearch version
-			"access_tags":               permanentResources["accessTags"],
+			"elasticsearch_version":      "8.12", // Always lock this test into the latest supported elasticsearch version
+			"access_tags":                permanentResources["accessTags"],
 			"existing_kms_instance_guid": permanentResources["hpcs_south"],
-			"kms_key_crn":               permanentResources["hpcs_south_root_key_crn"],
+			"kms_key_crn":                permanentResources["hpcs_south_root_key_crn"],
 		},
 		CloudInfoService: sharedInfoSvc,
 	})
