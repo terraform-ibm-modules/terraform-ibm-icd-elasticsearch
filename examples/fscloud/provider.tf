7f27ab9e2df6c166f69c2403552cdcf74bb4e201
AayushAbhyarthi <aayushabhyarthi4444@gmail.com>
2024-06-17 09:12:54 +0000

    pre-commit changes

--- examples/fscloud/provider.tf
+++ examples/fscloud/provider.tf
@@ -2,11 +2,3 @@ provider "ibm" {
   ibmcloud_api_key = var.ibmcloud_api_key
   region           = var.region
 }
-
-provider "restapi" {
-  uri = "https://"
-  headers = {
-    "Content-Type" = "application/json"
-  }
-  write_returns_object = true
-}
