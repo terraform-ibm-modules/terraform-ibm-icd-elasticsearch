e7607736f83e1e8b13470e034ec816b0dc43c706
Khuzaima-Shakeel <Khuzaima.Shakeel@ibm.com>
2024-08-02 17:29:46 +0530

    fix: added elasticsearch_index and elasticsearch_cluster_settings resource block to basic example

--- examples/basic/version.tf
+++ examples/basic/version.tf
@@ -6,5 +6,16 @@ terraform {
       source  = "IBM-Cloud/ibm"
       version = "1.65.0"
     }
+    # The elasticsearch provider is not actually required by the module itself, just this example, so OK to use ">=" here instead of locking into a version
+    elasticsearch = {
+      source  = "phillbaker/elasticsearch"
+      version = ">= 2.0.7"
+    }
+
+    # The time provider is not actually required by the module itself, just this example, so OK to use ">=" here instead of locking into a version
+    time = {
+      source  = "hashicorp/time"
+      version = ">= 0.9.1"
+    }
   }
 }
