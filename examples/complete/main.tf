6e0464bf3ef98f98a3e57096e9b319c95ce9d527
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2024-08-03 09:31:40 +0100

    fix(deps): update terraform terraform-ibm-modules/kms-all-inclusive/ibm to v4.15.3 (#249)

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -20,7 +20,7 @@ module "resource_group" {
 
 module "key_protect_all_inclusive" {
   source            = "terraform-ibm-modules/kms-all-inclusive/ibm"
-  version           = "4.15.2"
+  version           = "4.15.3"
   resource_group_id = module.resource_group.resource_group_id
   # Only us-south, eu-de backup encryption keys are supported. See https://cloud.ibm.com/docs/cloud-databases?topic=cloud-databases-key-protect&interface=ui#key-byok for details.
   # Note: Database instance and Key Protect must be created on the same region.
