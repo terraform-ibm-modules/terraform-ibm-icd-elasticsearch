d41c7614f0c11c27c017f140ea64c6ce2e27c384
akocbek@ie.ibm.com <akocbek@ie.ibm.com>
2024-10-03 11:25:37 +0100

    fix: small fixes around input DA variables

--- solutions/standard/outputs.tf
+++ solutions/standard/outputs.tf
@@ -4,7 +4,7 @@
 
 output "id" {
   description = "Elasticsearch instance id"
-  value       = local.use_existing_db_instance ? data.ibm_database.existing_db_instance[0].id : module.elasticsearch[0].id
+  value       = local.use_existing_elasticsearch_instance ? data.ibm_database.existing_db_instance[0].id : module.elasticsearch[0].id
 }
 
 output "guid" {
@@ -14,39 +14,39 @@ output "guid" {
 
 output "version" {
   description = "Elasticsearch instance version"
-  value       = local.use_existing_db_instance ? data.ibm_database.existing_db_instance[0].version : module.elasticsearch[0].version
+  value       = local.use_existing_elasticsearch_instance ? data.ibm_database.existing_db_instance[0].version : module.elasticsearch[0].version
 }
 
 output "crn" {
   description = "Elasticsearch instance crn"
-  value       = local.use_existing_db_instance ? var.existing_elastic_search_instance_crn : module.elasticsearch[0].crn
+  value       = local.use_existing_elasticsearch_instance ? var.existing_elasticsearch_instance_crn : module.elasticsearch[0].crn
 }
 
 output "cbr_rule_ids" {
   description = "CBR rule ids created to restrict Elasticsearch"
-  value       = local.use_existing_db_instance ? null : module.elasticsearch[0].cbr_rule_ids
+  value       = local.use_existing_elasticsearch_instance ? null : module.elasticsearch[0].cbr_rule_ids
 }
 
 output "service_credentials_json" {
   description = "Service credentials json map"
-  value       = local.use_existing_db_instance ? null : module.elasticsearch[0].service_credentials_json
+  value       = local.use_existing_elasticsearch_instance ? null : module.elasticsearch[0].service_credentials_json
   sensitive   = true
 }
 
 output "service_credentials_object" {
   description = "Service credentials object"
-  value       = local.use_existing_db_instance ? null : module.elasticsearch[0].service_credentials_object
+  value       = local.use_existing_elasticsearch_instance ? null : module.elasticsearch[0].service_credentials_object
   sensitive   = true
 }
 
 output "hostname" {
   description = "Elasticsearch instance hostname"
-  value       = local.use_existing_db_instance ? data.ibm_database_connection.existing_connection[0].https[0].hosts[0].hostname : module.elasticsearch[0].hostname
+  value       = local.use_existing_elasticsearch_instance ? data.ibm_database_connection.existing_connection[0].https[0].hosts[0].hostname : module.elasticsearch[0].hostname
 }
 
 output "port" {
   description = "Elasticsearch instance port"
-  value       = local.use_existing_db_instance ? data.ibm_database_connection.existing_connection[0].https[0].hosts[0].port : module.elasticsearch[0].port
+  value       = local.use_existing_elasticsearch_instance ? data.ibm_database_connection.existing_connection[0].https[0].hosts[0].port : module.elasticsearch[0].port
 }
 
 output "secrets_manager_secrets" {
