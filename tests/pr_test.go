edb15430626a381ee9790a50ac58edb8df6e2daa
Akash Kumar <akash.kumar@ibm.com>
2024-11-05 23:41:33 +0530

    addressed review comments

--- tests/pr_test.go
+++ tests/pr_test.go
@@ -193,7 +193,6 @@ func TestRunStandardSolutionSchematics(t *testing.T) {
 		{Name: "admin_pass_sm_secret_group", Value: options.Prefix, DataType: "string"},
 		{Name: "admin_pass_sm_secret_name", Value: options.Prefix, DataType: "string"},
 		{Name: "enable_kibana_dashboard", Value: true, DataType: "bool"},
-		{Name: "existing_code_engine_project_name", Value: options.Prefix, DataType: "string"},
 	}
 	err := options.RunSchematicTest()
 	assert.Nil(t, err, "This should not have errored")
