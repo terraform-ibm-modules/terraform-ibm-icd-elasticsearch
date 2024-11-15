947f389086555af9a1b2bf0647ad886517a23d83
shemau <shemau@gmail.com>
2024-11-15 17:22:56 +0000

    fix: bump CBR and IBM provider (#339)
    - Update terraform-ibm-modules/cbr/ibm to v1.28.1
    - Update required provider version to >= 1.70.0 to pick up the fix for this provider issue: [ibm_cbr_zone] Error: Provider produced inconsistent result after apply IBM-Cloud/terraform-provider-ibm#5590

--- version.tf
+++ version.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     ibm = {
       source  = "ibm-cloud/ibm"
-      version = ">= 1.68.1, <2.0.0"
+      version = ">= 1.70.0, <2.0.0"
     }
     null = {
       source  = "hashicorp/null"
