17d35047a083d8588c742c87740fb6f0880a0a8e
Jordan-Williams2 <Jordan.Williams2@ibm.com>
2024-12-06 01:37:38 +0000

    fix: change count condition

--- examples/fscloud/main.tf
+++ examples/fscloud/main.tf
@@ -80,21 +80,22 @@ module "key_protect_all_inclusive" {
 ##############################################################################
 
 module "elasticsearch" {
-  source                     = "../../modules/fscloud"
-  resource_group_id          = module.resource_group.resource_group_id
-  name                       = "${var.prefix}-elasticsearch"
-  region                     = var.region
-  tags                       = var.resource_tags
-  access_tags                = var.access_tags
-  kms_key_crn                = var.kms_key_crn
-  existing_kms_instance_guid = var.existing_kms_instance_guid
-  elasticsearch_version      = var.elasticsearch_version
-  service_credential_names   = var.service_credential_names
-  auto_scaling               = var.auto_scaling
-  member_host_flavor         = "b3c.4x16.encrypted"
-  backup_encryption_key_crn  = module.key_protect_all_inclusive.keys["icd.backup-${var.prefix}-elasticsearch"].crn
-  backup_crn                 = var.backup_crn
-  enable_elser_model         = var.enable_elser_model
+  source                           = "../../modules/fscloud"
+  resource_group_id                = module.resource_group.resource_group_id
+  name                             = "${var.prefix}-elasticsearch"
+  region                           = var.region
+  tags                             = var.resource_tags
+  access_tags                      = var.access_tags
+  kms_key_crn                      = var.kms_key_crn
+  existing_kms_instance_guid       = var.existing_kms_instance_guid
+  elasticsearch_version            = var.elasticsearch_version
+  service_credential_names         = var.service_credential_names
+  auto_scaling                     = var.auto_scaling
+  member_host_flavor               = "b3c.4x16.encrypted"
+  backup_encryption_key_crn        = module.key_protect_all_inclusive.keys["icd.backup-${var.prefix}-elasticsearch"].crn
+  use_custom_backup_encryption_key = true
+  backup_crn                       = var.backup_crn
+  enable_elser_model               = var.enable_elser_model
   cbr_rules = [
     {
       description      = "${var.prefix}-elasticsearch access only from vpc"
