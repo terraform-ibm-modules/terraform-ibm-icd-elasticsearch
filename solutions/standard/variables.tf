3e7f7e8454ba4d78c8ee61c14a652c90ae62cbc3
Vipin654 <77929205+Vipin654@users.noreply.github.com>
2025-02-13 17:32:58 +0530

    updated description of cbr_rules variable

--- solutions/standard/variables.tf
+++ solutions/standard/variables.tf
@@ -415,6 +415,6 @@ variable "cbr_rules" {
       }))
     })))
   }))
-  description = "(Optional, list) List of context-based restrictions rules to create. [Learn more](https://github.com/terraform-ibm-modules/terraform-ibm-kms-all-inclusive/tree/main/solutions/standard/DA-cbr_rules.md)"
+  description = "(Optional, list) List of context-based restrictions rules to create."
   default     = []
 }
