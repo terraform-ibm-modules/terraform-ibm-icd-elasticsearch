cb25aa8f06629e47d62c0397197ad2cc38f3bc9e
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2025-05-17 02:56:48 +0000

    fix(deps): update terraform dependencies

--- solutions/standard/main.tf
+++ solutions/standard/main.tf
@@ -40,7 +40,7 @@ module "kms" {
   }
   count                       = local.create_new_kms_key
   source                      = "terraform-ibm-modules/kms-all-inclusive/ibm"
-  version                     = "5.1.2"
+  version                     = "5.1.4"
   create_key_protect_instance = false
   region                      = local.kms_region
   existing_kms_instance_crn   = var.existing_kms_instance_crn
@@ -419,7 +419,7 @@ module "secrets_manager_service_credentials" {
   count                       = var.existing_secrets_manager_instance_crn == null ? 0 : 1
   depends_on                  = [time_sleep.wait_for_es_authorization_policy]
   source                      = "terraform-ibm-modules/secrets-manager/ibm//modules/secrets"
-  version                     = "2.2.6"
+  version                     = "2.2.7"
   existing_sm_instance_guid   = local.existing_secrets_manager_instance_guid
   existing_sm_instance_region = local.existing_secrets_manager_instance_region
   endpoint_type               = var.existing_secrets_manager_endpoint_type
