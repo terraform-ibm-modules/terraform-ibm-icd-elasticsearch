6469d54161dfddcaf42c316191f371b1fde4f525
Akash Kumar <akash.kumar@ibm.com>
2025-05-24 10:48:23 +0530

    updated var des

--- solutions/standard/variables.tf
+++ solutions/standard/variables.tf
@@ -452,6 +452,6 @@ variable "cbr_code_engine_kibana_rules" {
       }))
     })))
   }))
-  description = "(Optional, list) List of context-based restrictions rules to create. [Learn more](https://github.com/terraform-ibm-modules/terraform-ibm-icd-elasticsearch/tree/main/solutions/standard/DA-cbr_rules.md)"
+  description = "(Optional, list) List of context-based restrictions rules to create for the Kibana dashboard. [Learn more](https://github.com/terraform-ibm-modules/terraform-ibm-icd-elasticsearch/tree/main/solutions/standard/DA-cbr_rules.md)"
   default     = []
 }
