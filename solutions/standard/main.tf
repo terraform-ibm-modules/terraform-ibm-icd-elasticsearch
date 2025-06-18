414f320dd082ccf22332d58ee592842a87928e9e
Akash Kumar <akash.kumar@ibm.com>
2025-06-18 16:38:12 +0530

    addressed review comments

--- solutions/standard/main.tf
+++ solutions/standard/main.tf
@@ -303,8 +303,8 @@ module "elasticsearch" {
   access_tags                       = var.access_tags
   tags                              = var.tags
   admin_pass                        = local.admin_pass
-  members                           = var.members
   users                             = local.all_users
+  members                           = var.members
   member_host_flavor                = var.member_host_flavor
   member_memory_mb                  = var.member_memory_mb
   member_disk_mb                    = var.member_disk_mb
