c58fb28ddf36641ad9908ca22514870240d40693
Akash Kumar <akash.kumar@ibm.com>
2025-07-01 22:38:17 +0530

    Revert "addressed review comments"
    This reverts commit 414f320dd082ccf22332d58ee592842a87928e9e.

--- solutions/fully-configurable/main.tf
+++ solutions/fully-configurable/main.tf
@@ -302,8 +302,8 @@ module "elasticsearch" {
   access_tags                       = var.access_tags
   tags                              = var.resource_tags
   admin_pass                        = local.admin_pass
-  users                             = local.all_users
   members                           = var.members
+  users                             = local.all_users
   member_host_flavor                = var.member_host_flavor
   memory_mb                         = var.member_memory_mb
   disk_mb                           = var.member_disk_mb
