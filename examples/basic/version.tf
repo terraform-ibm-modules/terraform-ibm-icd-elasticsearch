7002a5646ea5d8aa0acfc67d4ef1f205e9ff4dcb
Aditya Ranjan <48973656+Aditya-ranjan-16@users.noreply.github.com>
2025-09-30 14:41:43 +0530

    test: temporarily disabling index creation to unblock the pipeline (#531)

--- examples/basic/version.tf
+++ examples/basic/version.tf
@@ -12,9 +12,9 @@ terraform {
       version = ">= 2.0.7"
     }
     # The time provider is not actually required by the module itself, just this example, so OK to use ">=" here instead of locking into a version
-    time = {
-      source  = "hashicorp/time"
-      version = ">= 0.9.1"
-    }
+    # time = {
+    #   source  = "hashicorp/time"
+    #   version = ">= 0.9.1"
+    # }
   }
 }
