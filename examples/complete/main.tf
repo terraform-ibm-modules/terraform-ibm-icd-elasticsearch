020bf17e77b73465eb2e9ba8794d0fd3c15ff50d
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2025-10-25 20:00:44 +0000

    fix(deps): update terraform terraform-ibm-modules/secrets-manager/ibm to v2.11.3

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -98,7 +98,7 @@ module "icd_elasticsearch" {
 module "secrets_manager" {
   count                = var.existing_sm_instance_guid == null ? 1 : 0
   source               = "terraform-ibm-modules/secrets-manager/ibm"
-  version              = "2.11.2"
+  version              = "2.11.3"
   resource_group_id    = module.resource_group.resource_group_id
   region               = var.region
   secrets_manager_name = "${var.prefix}-secrets-manager"
