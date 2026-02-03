b77bd58dc11ae77d4940e8a10b201215eac9583d
Aditya Ranjan <48973656+Aditya-ranjan-16@users.noreply.github.com>
2026-02-03 11:22:11 +0530

    Revert "test: temporarily disabling index creation to unblock the pipeline (#…"
    This reverts commit 7002a5646ea5d8aa0acfc67d4ef1f205e9ff4dcb.

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -38,24 +38,21 @@ module "database" {
 
 # wait 60 secs to allow IAM credential access to kick in before configuring instance
 # without the wait, you can intermittently get "Error 401 (Unauthorized)"
+resource "time_sleep" "wait" {
+  depends_on      = [module.database]
+  create_duration = "60s"
+}
 
-# Temporarily disabling index creation due to an know issue blocking the pipeline : https://github.ibm.com/GoldenEye/issues/issues/16245.
+resource "elasticsearch_index" "test" {
+  depends_on         = [time_sleep.wait]
+  name               = "terraform-test"
+  number_of_shards   = 1
+  number_of_replicas = 1
+  force_destroy      = true
+}
 
-# resource "time_sleep" "wait" {
-#   depends_on      = [module.database]
-#   create_duration = "60s"
-# }
-
-# resource "elasticsearch_index" "test" {
-#   depends_on         = [time_sleep.wait]
-#   name               = "terraform-test"
-#   number_of_shards   = 1
-#   number_of_replicas = 1
-#   force_destroy      = true
-# }
-
-# resource "elasticsearch_cluster_settings" "global" {
-#   depends_on                  = [time_sleep.wait]
-#   cluster_max_shards_per_node = 10
-#   action_auto_create_index    = "my-index-000001,index10,-index1*,+ind*"
-# }
+resource "elasticsearch_cluster_settings" "global" {
+  depends_on                  = [time_sleep.wait]
+  cluster_max_shards_per_node = 10
+  action_auto_create_index    = "my-index-000001,index10,-index1*,+ind*"
+}
