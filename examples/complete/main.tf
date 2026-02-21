ec612f74e165f2b7f4e8d07325df5fd48213ba74
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2026-02-21 12:15:36 +0000

    fix(deps): pin dependencies (#630)

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -110,7 +110,7 @@ module "secrets_manager" {
 # Add a Secrets Group to the secret manager instance
 module "secrets_manager_secrets_group" {
   source               = "terraform-ibm-modules/secrets-manager-secret-group/ibm"
-  version              = "1.4.5"
+  version              = "1.4.6"
   region               = local.secrets_manager_region
   secrets_manager_guid = local.secrets_manager_guid
   #tfsec:ignore:general-secrets-no-plaintext-exposure
