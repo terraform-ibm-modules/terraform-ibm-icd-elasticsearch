3d69809af0a5ac3ec1b2852cd1e7b9c897201472
Arya Girish K <arya.girish.k@ibm.com>
2026-03-10 15:25:08 +0530

    fix: Resolved precommit error

--- main.tf
+++ main.tf
@@ -412,7 +412,7 @@ locals {
 resource "terraform_data" "install_required_binaries" {
   count = var.install_required_binaries ? 1 : 0
   triggers_replace = {
-  file_changed = md5(var.elser_model_type)
+    file_changed = md5(var.elser_model_type)
   }
 
   provisioner "local-exec" {
