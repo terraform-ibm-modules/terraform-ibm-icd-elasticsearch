80a89d550e170180a86c42abbedd1a065081fb6d
Piyush Kumar Sahu <Piyush.Kumar.Sahu@ibm.com>
2026-08-14 14:40:55 +0530

    Update solutions/fully-configurable-gen2/variables.tf
    Co-authored-by: Aashiq Jacob <122446118+Aashiq-J@users.noreply.github.com>

--- solutions/fully-configurable-gen2/variables.tf
+++ solutions/fully-configurable-gen2/variables.tf
@@ -194,7 +194,7 @@ variable "existing_kms_instance_crn" {
   validation {
     condition = anytrue([
       var.existing_kms_instance_crn == null,
-      can(regex("^crn:v\\d:(.*:){2}(kms|hs-crypto):(.*:)([aos]\\/[\\w_\\-]+):[0-9a-fA-F]{8}(?:-[0-9a-fA-F]{4}){3}-[0-9a-fA-F]{12}::$", var.existing_kms_instance_crn))
+      can(regex("^crn:v\\d:(.*:){2}(kms):(.*:)([aos]\\/[\\w_\\-]+):[0-9a-fA-F]{8}(?:-[0-9a-fA-F]{4}){3}-[0-9a-fA-F]{12}::$", var.existing_kms_instance_crn))
     ])
     error_message = "The value provided for 'existing_kms_instance_crn' is not valid."
   }
