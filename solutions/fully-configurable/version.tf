1491135ef7d7aaa81564ac57005d697d450392e1
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2026-08-15 05:22:01 +0000

    fix(deps): update dependencies

--- solutions/fully-configurable/version.tf
+++ solutions/fully-configurable/version.tf
@@ -8,7 +8,7 @@ terraform {
     }
     time = {
       source  = "hashicorp/time"
-      version = "0.14.0"
+      version = "0.14.1"
     }
     random = {
       source  = "hashicorp/random"
@@ -16,7 +16,7 @@ terraform {
     }
     external = {
       source  = "hashicorp/external"
-      version = "2.4.0"
+      version = "2.4.1"
     }
   }
 }
